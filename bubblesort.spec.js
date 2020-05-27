16ededff66c153894bc01959957f31b67d251044
Kristy Li <kristyli20.0@gmail.com>
2020-05-27 14:36:40 -0400

    Update bubblesort.spec.js

--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,21 +1,21 @@
 describe("Bubble Sort", function () {
   it("handles an empty array", function () {
-    expect(arr.bubbleSort([])).toEqual([]);
+    expect(obj.bubbleSort([])).toEqual([]);
   });
   it("handles an array", function () {
-    expect(arr.bubbleSort([4, 3, 2, 1])).toEqual([1, 2, 3, 4]);
+    expect(obj.bubbleSort([4, 3, 2, 1])).toEqual([1, 2, 3, 4]);
   });
 });
 
 describe("Bubble Sort Swap", function () {
-  const nums = [4,3,2,1];
+  const nums = [4, 3, 2, 1];
 
   beforeAll(function () {
-    spyOn(arr, 'swap').and.callThrough();
+    spyOn(obj, "swap").and.callThrough();
   });
 
   it("sorting the array involves exactly 6 swaps", function () {
-    arr.bubbleSort(nums);
-    expect(arr.swap.calls.count()).toEqual(6);
+    obj.bubbleSort(nums);
+    expect(obj.swap.calls.count()).toEqual(6);
   });
 });
